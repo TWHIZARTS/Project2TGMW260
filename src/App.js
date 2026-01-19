06a15ad3084c6ccc7fa842571afea3ea398d5e15
Tori Whisenant <twhiznt@gmail.com>
2026-01-19 18:16:19 -0500

    Started making the table. Had a bit of ttrouble with setData, but the console is printing out the array fine.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// MADE BY: T.G.M.W.
+// 1/16/26
+// For csc260
+// centreterm
+
+
+
 import { useEffect, useState} from "react";
 
 function App() {
 
-  
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [data,setData]=useState('Loading Data')
-
+ const [data,setData]=useState([]);
 
   useEffect(() =>{
     async function fetchData()
@@ -19,43 +20,42 @@ function App() {
 
     if (response.ok) 
       {
-        const result = await response.json();
-        console.log(result)
-
-        setData(result.data);
-    
+      const result = await response.json();
+      console.log(result.Grants.Grant)
+      setData(result.Grants.Grant)
       }
 
-      return({data})
+  
     }
     fetchData()
-
+    function drawTable(props){
+      <table>
+      <td> Project Title</td>
+      <td>Institution</td>
+      </table>}
+      
 
   },[]);
 
 // YES YOU HAVE YOUR EMPTY ARRAY, TORI
 
-  return (
-    
-    
-    <div className="App">
-      fetchData();
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> HEY THIS IS THE BIT YOU EDIT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return (<div className="App">
+  <>
+  <table>
+    <thead>
+      <td> Project Title</td>
+      <td>Institution</td>
+    </thead>
+    grant
+    <tbody>
+
+ 
+    </tbody>
+    </table>
+  </>
+
+
+
+</div>);}
 
 export default App;
